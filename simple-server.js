01537ff213fbcde34e9fa9d4ede444b131993fef
Xosé Ramón Larroy Becerra <130966521+Xoselb@users.noreply.github.com>
2025-10-24 12:22:11 +0200

    Problème résolu

--- simple-server.js
+++ simple-server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Servir les fichiers statiques depuis le dossier public
 app.use(express.static('.'));
